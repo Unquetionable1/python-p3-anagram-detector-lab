d43b98194ef5d92380830e175ce42f06287194ef
Edmound Njoori <eddnjoori@gmail.com>
2024-06-01 07:12:18 +0300

    Create a class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.letters = sorted([letter for letter in word])
+
+    def match(self, words_list):
+        matched = []
+        for word in words_list:
+            if sorted([letter for letter in word]) == self.letters:
+                matched.append(word)
+        return matched
+
+
